ba66d0101d96f56b9c52e6ce543bb323ed313720
ChauveSouris <desseobscurite@gmail.com>
2023-03-07 01:21:38 +0400

    Imlemented save and load date

--- Pet_Project_1/Model.swift
+++ Pet_Project_1/Model.swift
@@ -7,29 +7,54 @@
 
 import Foundation
 
-var ToDoList: [[String: Any]] = [["Name": "1", "isComplited": false], ["Name": "2", "isComplited": true]]
-
-func addList(nameList: String, isComplited: Bool = false) {
-    ToDoList.append(["Name": nameList, "isComplited": isComplited]) //Добавление нового элемента
-    saveData()
-}
-
-func removeList(at index: Int) {
-    ToDoList.remove(at: index) //Удаление элемента по индексу
-    saveData()
-}
-
-func changeState (at list: Int) -> Bool {
-    ToDoList[list]["isComplited"] = !(ToDoList[list]["isComplited"] as! Bool)
-    saveData()
-    return ToDoList[list]["isComplited"] as! Bool
-} //Выбор галочки в списке дел 
-
-func saveData() {
-    print ("Save data!")
+var ToDoList: [[String: Any]] {
+    set {
+        UserDefaults.standard.set(newValue, forKey: "ToDoData")
+        UserDefaults.standard.synchronize()
+    }
     
+    get {
+        if let array = UserDefaults.standard.array(forKey: "ToDoData")
+            as? [[String: Any]] {
+            return array
+        } else {
+            return []
+        }
+    }
 }
-
-func loadData() {
     
-}
+    func addList(nameList: String, isComplited: Bool = false) {
+        ToDoList.append(["Name": nameList, "isComplited": isComplited]) //Добавление нового элемента, реализация через словарь
+        //saveData()
+    }
+    
+    func removeList(at index: Int) {
+        ToDoList.remove(at: index) //Удаление элемента по индексу
+        //saveData()
+    }
+    
+    func changeState (at list: Int) -> Bool {
+        ToDoList[list]["isComplited"] = !(ToDoList[list]["isComplited"] as! Bool)
+        //saveData()
+        return ToDoList[list]["isComplited"] as! Bool
+    } //Выбор галочки в списке дел
+    
+    /*
+     
+     func saveData() {
+     UserDefaults.standard.set(ToDoList, forKey: "ToDoData")
+     UserDefaults.standard.synchronize()
+     print ("Save data!")
+     } //Сохранение данных
+     
+     func loadData() {
+     if let array = UserDefaults.standard.array(forKey: "ToDoData")
+     as? [[String: Any]] {
+     ToDoList = array
+     } else {
+     ToDoList = []
+     }
+     } //Загрузка данных
+     
+     */
+    
